8ce5303ce5913181c01774a94a583bc60f05f67c
JosiahMbithi001 <mbithishucks@gmail.com>
2023-02-28 04:08:23 -0800

    2-Insert_right

--- binary_trees.h
+++ binary_trees.h
@@ -34,4 +34,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* Task 1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/* Task 2 */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREE_H */
