63a63359c9ac59a912ee6380fc8140154c902289
JosiahMbithi001 <mbithishucks@gmail.com>
2023-03-02 11:55:31 -0800

    17-Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,14 +9,18 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent)
+	binary_tree_t *temp = NULL;
+
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left && node->parent->left != NULL)
-		return (node->parent->left);
+	if (node->parent->left == NULL)
+		temp = node->parent->right;
+	else
+		temp = node->parent->left;
 
-	if (node->parent->right && node->parent->right != NULL)
-		return (node->parent->right);
+	if (temp == NULL)
+		return (NULL);
 
-	return (NULL);
+	return (temp);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -64,7 +64,7 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 /* Task 16 Tree is perfect */
 int binary_tree_is_perfect(const binary_tree_t *tree);
-/* Tssk 17 Node Sibling */
+/* Task 17 Node Sibling */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /* Task 18 Uncle of Node */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
